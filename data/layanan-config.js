bdf5d00198ae546b833be90d8c49d72aee9e0e2b
kuncoro-0927 <khitanhesthik@gmail.com>
2026-09-14 18:01:01 +0700

    upd folder structure

---
+++ data/layanan-config.js
@@ -0,0 +1,14 @@
+const LAYANAN_CONFIG = {
+  dana: { nama: "DANA", logo: "/assets/images/e-wallet/Dana.svg" },
+  gopay: { nama: "GoPay", logo: "/assets/images/e-wallet/Gopay.svg" },
+  ovo: { nama: "OVO", logo: "/assets/images/e-wallet/OVO.svg" },
+  shopeepay: { nama: "GoPay", logo: "/assets/images/e-wallet/ShopeePay.svg" },
+  pln: { nama: "PLN", logo: "/assets/images/PLN.svg" },
+  bpjs: { nama: "BPJS Kesehatan", logo: "/assets/images/BPJS.svg" },
+  pulsa: { nama: "Pulsa", logo: "/assets/img/logo-pulsa.png" },
+  paket_data: { nama: "Paket Data" },
+};
+
+const PROVIDER_LOGO = {
+  XL: "/assets/images/Logo-XL.svg",
+};
